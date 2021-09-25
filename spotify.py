3ac56e5ec99538e1430d465724986b350da651dc
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-09-25 14:28:34 +0200

    Add play from playlist

---
+++ spotify.py
@@ -0,0 +1,20 @@
+import json
+import requests
+from secrets import SPOTIFY_OATH_TOKEN
+
+url = "https://api.spotify.com/v1/me/player/play"
+data = json.dumps({
+  "context_uri": "spotify:playlist:37i9dQZF1DX1s9knjP51Oa"
+})
+
+response = requests.put(
+    url, 
+    data,
+    headers={
+        "Authorization": "Bearer {}".format(SPOTIFY_OATH_TOKEN),
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+)
+
+print(response)
