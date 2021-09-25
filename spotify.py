b5bb565a4c777078361521fd25f1b4a77b33bba9
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-09-25 15:07:29 +0200

    Fix playback if no active device

--- spotify.py
+++ spotify.py
@@ -1,29 +1,32 @@
 import json
 import requests
-from secrets import SPOTIFY_OATH_TOKEN
+from secrets import SPOTIFY_OATH_TOKEN, SPOTIFY_DEVICE_ID
 
 def make_request(url: str, data: str = ""):
     response = requests.put(
         url, 
         data,
         headers={
-            "Authorization": "Bearer {}".format(SPOTIFY_OATH_TOKEN),
+            "Authorization": f"Bearer {SPOTIFY_OATH_TOKEN}",
             "Accept": "application/json",
             "Content-Type": "application/json"
         }
     )
 
-    print(response)
+    print(response.content)
+
+def play_music():
+    # Toggle Shuffle
+    shuffle_endpoint = f"https://api.spotify.com/v1/me/player/shuffle?state=true&device_id={SPOTIFY_DEVICE_ID}"
+    make_request(shuffle_endpoint)
 
 
-# Toggle Shuffle
-shuffle_endpoint = "https://api.spotify.com/v1/me/player/shuffle?state=true"
-make_request(shuffle_endpoint)
+    # Play playlist
+    playback_url = f"https://api.spotify.com/v1/me/player/play?device_id={SPOTIFY_DEVICE_ID}"
+    playback_data = json.dumps({
+    "context_uri": "spotify:playlist:37i9dQZF1DX3Ogo9pFvBkY"
+    })
+    make_request(playback_url, playback_data)
 
-
-# Play playlist
-playback_url = "https://api.spotify.com/v1/me/player/play"
-playback_data = json.dumps({
-  "context_uri": "spotify:playlist:37i9dQZF1DX3Ogo9pFvBkY"
-})
-make_request(playback_url, playback_data)
+if __name__ == "__main__":
+    play_music()
