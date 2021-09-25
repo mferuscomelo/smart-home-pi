f41a65dd2a9e4d5f349e20dc45c9d5313639d5d7
Milan Ferus-Comelo <51026229+mferuscomelo@users.noreply.github.com>
2021-09-25 14:49:06 +0200

    Shuffle playlist on play

--- spotify.py
+++ spotify.py
@@ -2,19 +2,28 @@ import json
 import requests
 from secrets import SPOTIFY_OATH_TOKEN
 
-url = "https://api.spotify.com/v1/me/player/play"
-data = json.dumps({
+def make_request(url: str, data: str = ""):
+    response = requests.put(
+        url, 
+        data,
+        headers={
+            "Authorization": "Bearer {}".format(SPOTIFY_OATH_TOKEN),
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    )
+
+    print(response)
+
+
+# Toggle Shuffle
+shuffle_endpoint = "https://api.spotify.com/v1/me/player/shuffle?state=true"
+make_request(shuffle_endpoint)
+
+
+# Play playlist
+playback_url = "https://api.spotify.com/v1/me/player/play"
+playback_data = json.dumps({
   "context_uri": "spotify:playlist:37i9dQZF1DX3Ogo9pFvBkY"
 })
-
-response = requests.put(
-    url, 
-    data,
-    headers={
-        "Authorization": "Bearer {}".format(SPOTIFY_OATH_TOKEN),
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-)
-
-print(response)
+make_request(playback_url, playback_data)
